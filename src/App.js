51374634b9e8bb48291623f816b0b3e6bd3498eb
ShunshunL <shunshunlyu@protonmail.com>
2020-05-19 14:25:18 -0700

    logged in user cannot access signin signup page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux'
 import './App.css';
 import Homepage from './pages/homepage/homepage.component'
@@ -41,15 +41,19 @@ class App extends React.Component  {
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={PetPage} />
-          <Route path="/signin" component={SignInSignUp} />
+          <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to='/' />) : <SignInSignUp />} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
