b3c489cc456af22b837960d44ebe3f4f85cebc9a
ShunshunL <shunshunlyu@protonmail.com>
2020-05-22 12:33:40 -0700

    removed logger for production

---
+++ build/precache-manifest.059f328871570f6deebba2fae59d396e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebdfb68703fb165ac605843db1873b70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b294c8cefb97ff24274c",
+    "url": "/static/css/main.e7c1d1d9.chunk.css"
+  },
+  {
+    "revision": "eb9b2217b7254b94c173",
+    "url": "/static/js/2.e73f361e.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.e73f361e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b294c8cefb97ff24274c",
+    "url": "/static/js/main.077b588e.chunk.js"
+  },
+  {
+    "revision": "b89f0e56c70aa13a63ad",
+    "url": "/static/js/runtime-main.266c6519.js"
+  },
+  {
+    "revision": "4021430f346b8b94db62745679f395f7",
+    "url": "/static/media/background.4021430f.jpg"
+  },
+  {
+    "revision": "e61fe2854fef657ab5713e03316ad6b2",
+    "url": "/static/media/checkout.e61fe285.svg"
+  },
+  {
+    "revision": "3f0aa5f739a8591881819339e1346734",
+    "url": "/static/media/logo.3f0aa5f7.svg"
+  }
+]);
